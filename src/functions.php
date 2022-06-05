ee78ddd47303ada20f0a7708f786f3352c18f411
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-06-05 15:15:00 -0400

    Loads editor css.

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,17 @@
 
 namespace Transgression;
 
+// Includes
+require_once 'inc/class-applications.php';
+
+function init() {
+	add_theme_support( 'editor-styles' );
+	add_editor_style( 'editor.css' );
+
+	Applications::init();
+}
+add_action( 'init', cb( 'init' ) );
+
 function styles() {
 	wp_enqueue_style( 'transgression-styles', get_theme_file_uri( 'style.css' ) , [], null, 'screen' );
 }
--- wp-config.php
+++ wp-config.php
@@ -1,23 +1,24 @@
-<?php
+<?php declare( strict_types=1 );
+// phpcs:disable NeutronStandard.Constants.DisallowDefine.Define
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wordpress');
+define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password' );
+define( 'DB_PASSWORD', 'password' );
 
 /** MySQL hostname */
-define('DB_HOST', 'database');
+define( 'DB_HOST', 'database' );
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
+define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+define( 'DB_COLLATE', '' );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -28,14 +29,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '34[>*FMTdM+[$l(Q+t>2O q]*`6y/d-KdT^*-zo7q+f><NlbyCuEq){8gQqb_;;G');
-define('SECURE_AUTH_KEY',  ' fw?9[j!.1NPZ#tFlFM1+sgdmLMSM{y=fj?MWkEzvwf~7-P}]dsG&GIwY89]4xV(');
-define('LOGGED_IN_KEY',    'D%6t[A-{E/oxv|da?To1(}V8:h-y0=e9hh4jHEg|+#N]@xP}%|&3Z&`bN/_B_#B&');
-define('NONCE_KEY',        ':(ODd2,goEQ&LswN(GvK4C#W_!G{h9Y~OGbJqyK6n;+Rkbl&+gV&$1(1g>aU+SX>');
-define('AUTH_SALT',        ' `-t3RJ M OpQ%ql&P|/|3rr68ZWurZ/~^:v6(rU:`/#1fl{XRiX$L&+9,2+Q$pU');
-define('SECURE_AUTH_SALT', '/?X5xxvS|NN)|a`CcbAb}F.Up.DkK|R0t$?xFGe|v*C+@H>S*Z;Q!O.!PHz|6DvP');
-define('LOGGED_IN_SALT',   'kGRQ|byZ@+@Hk$E=|_NCwH}`?2x5rTO(yv-Y)*!t/SRW%+ag]bEHk~iqds3AYo;l');
-define('NONCE_SALT',       'cky<EHCe.)E|O-L`oNE>@LL]|j!||F0 3vex|gZ(;T&3Dav`U3W%+lL-Y r#`ua@');
+define( 'AUTH_KEY',         '34[>*FMTdM+[$l(Q+t>2O q]*`6y/d-KdT^*-zo7q+f><NlbyCuEq){8gQqb_;;G' );
+define( 'SECURE_AUTH_KEY',  ' fw?9[j!.1NPZ#tFlFM1+sgdmLMSM{y=fj?MWkEzvwf~7-P}]dsG&GIwY89]4xV(' );
+define( 'LOGGED_IN_KEY',    'D%6t[A-{E/oxv|da?To1(}V8:h-y0=e9hh4jHEg|+#N]@xP}%|&3Z&`bN/_B_#B&' );
+define( 'NONCE_KEY',        ':(ODd2,goEQ&LswN(GvK4C#W_!G{h9Y~OGbJqyK6n;+Rkbl&+gV&$1(1g>aU+SX>' );
+define( 'AUTH_SALT',        ' `-t3RJ M OpQ%ql&P|/|3rr68ZWurZ/~^:v6(rU:`/#1fl{XRiX$L&+9,2+Q$pU' );
+define( 'SECURE_AUTH_SALT', '/?X5xxvS|NN)|a`CcbAb}F.Up.DkK|R0t$?xFGe|v*C+@H>S*Z;Q!O.!PHz|6DvP' );
+define( 'LOGGED_IN_SALT',   'kGRQ|byZ@+@Hk$E=|_NCwH}`?2x5rTO(yv-Y)*!t/SRW%+ag]bEHk~iqds3AYo;l' );
+define( 'NONCE_SALT',       'cky<EHCe.)E|O-L`oNE>@LL]|j!||F0 3vex|gZ(;T&3Dav`U3W%+lL-Y r#`ua@' );
 
 /**#@-*/
 
@@ -45,13 +46,13 @@ define('NONCE_SALT',       'cky<EHCe.)E|O-L`oNE>@LL]|j!||F0 3vex|gZ(;T&3Dav`U3W%
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix = 'wp_';
 
 /**
  * Limits total Post Revisions saved per Post/Page.
  * Change or comment this line out if you would like to increase or remove the limit.
  */
-define('WP_POST_REVISIONS',  10);
+define( 'WP_POST_REVISIONS',  10 );
 
 /**
  * WordPress Localized Language, defaults to English.
@@ -61,7 +62,7 @@ define('WP_POST_REVISIONS',  10);
  * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
  * language support.
  */
-define('WPLANG', '');
+define( 'WPLANG', '' );
 
 /**
  * For developers: WordPress debugging mode.
@@ -71,14 +72,12 @@ define('WPLANG', '');
  * in their development environments.
  */
 define( 'WP_DEBUG_LOG', true );
-// define( 'WP_DEBUG_DISPLAY', true );
 
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+if ( !defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 
 /** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
-
+require_once( ABSPATH . 'wp-settings.php' );
