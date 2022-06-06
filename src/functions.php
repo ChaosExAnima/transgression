dffe3baba4daf379d0f2c5f2094bd36b721c82ee
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-06-05 20:45:05 -0400

    Adds email handling.

--- src/functions.php
+++ src/functions.php
@@ -4,9 +4,19 @@ namespace Transgression;
 
 // Includes
 require_once 'inc/class-applications.php';
+require_once 'inc/emails.php';
 
 if ( defined( 'JET_FORM_BUILDER_VERSION' ) && version_compare( JET_FORM_BUILDER_VERSION, '2.0.6', '>=' ) ) {
 	require_once 'inc/jetforms.php';
+}
+
+
+function theme_path( string $file = '' ): string {
+	$path = untrailingslashit( get_stylesheet_directory() );
+	if ( $file && file_exists( "{$path}/${file}" ) ) {
+		$path .= "/{$file}";
+	}
+	return $path;
 }
 
 function init() {
@@ -21,6 +31,10 @@ function styles() {
 	wp_enqueue_style( 'transgression-styles', get_theme_file_uri( 'style.css' ) , [], null, 'screen' );
 }
 add_action( 'wp_enqueue_scripts', cb( 'styles' ) );
+
+function log_error( \WP_Error $error ) {
+	error_log( implode( '; ', $error->get_error_messages() ) );
+}
 
 function cb( string $func ): Callable {
     return __NAMESPACE__ . '\\' . $func;
