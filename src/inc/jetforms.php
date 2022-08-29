21c0a8583a193ff7726e29eb7381d94363478f07
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-08-28 23:27:23 -0400

    Fix bug with missing emails not causing dupe email.

--- src/inc/jetforms.php
+++ src/inc/jetforms.php
@@ -7,9 +7,9 @@ use Jet_Form_Builder\Actions\Types\Base;
 use Jet_Form_Builder\Blocks\Block_Helper;
 use WP_Error;
 
-function before_application_insert( bool $return, array $source ): bool {
-	if ( isset( $source['meta_input']['email'] ) ) {
-		$email = trim( (string) $source['meta_input']['email'] );
+function before_application_insert( bool $return ): bool {
+	if ( isset( $_REQUEST['email'] ) ) {
+		$email = trim( (string) $_REQUEST['email'] );
 		$user_id = email_exists( $email );
 		if ( $user_id ) {
 			$result = Emails::instance()->send_user_email( $user_id, 'email_duplicate' );
@@ -21,7 +21,7 @@ function before_application_insert( bool $return, array $source ): bool {
 	}
 	return $return;
 }
-add_filter( 'jet-form-builder/action/insert-post/pre-check', cb( 'before_application_insert' ), 10, 2 );
+add_filter( 'jet-form-builder/action/insert-post/pre-check', cb( 'before_application_insert' ) );
 
 function after_application_insert( Base $action, Action_Handler $handler ) {
 	$form_id = $handler->get_form_id();
