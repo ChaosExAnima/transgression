2395ddf21a9a29cb84e0d1ce30a15ca7e9fe7312
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2022-09-18 13:17:05 -0400

    Adds comments to make Woo versioning system happy

--- src/woocommerce/loop/add-to-cart.php
+++ src/woocommerce/loop/add-to-cart.php
@@ -1,4 +1,11 @@
 <?php declare( strict_types=1 );
+/**
+ * Loop Add to Cart
+ *
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @package     WooCommerce\Templates
+ * @version     3.3.0
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
--- src/woocommerce/loop/no-products-found.php
+++ src/woocommerce/loop/no-products-found.php
@@ -1,4 +1,11 @@
 <?php declare( strict_types=1 );
+/**
+ * Displayed when no products are found matching the current query
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 2.0.0
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
