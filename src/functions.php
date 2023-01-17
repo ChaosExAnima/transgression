3c49055c2aa460794100f1aad56b94dd148c271e
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2023-01-16 21:07:51 -0500

    Removes last of plugin code and updates version to 1.0!

--- src/functions.php
+++ src/functions.php
@@ -2,14 +2,11 @@
 
 namespace TransgressionTheme;
 
+use WC_Product;
+
 function cb( string $func ): Callable {
     return __NAMESPACE__ . '\\' . $func;
 }
-
-// Includes
-require_once 'inc/helpers.php';
-require_once 'inc/jetforms.php';
-require_once 'inc/woocommerce.php';
 
 function init() {
 	add_theme_support( 'editor-styles' );
@@ -19,6 +16,16 @@ function init() {
 	remove_theme_support(  'wc-product-gallery-slider' );
 	remove_theme_support( 'wc-product-gallery-zoom' );
 	remove_theme_support( 'wc-product-gallery-lightbox' );
+
+	// Woo
+	if ( defined( 'WC_PLUGIN_FILE' ) ) {
+		add_filter( 'the_title', cb( 'filter_wc_title' ), 10, 2 );
+		add_action( 'woocommerce_checkout_order_review', cb( 'render_wc_clear_cart' ), 15 );
+
+		remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
+		remove_action( 'woocommerce_order_details_after_order_table', 'woocommerce_order_again_button' );
+		add_filter( 'wc_add_to_cart_message_html', '__return_empty_string' );
+	}
 }
 add_action( 'init', cb( 'init' ) );
 
@@ -35,16 +42,20 @@ function redirect() {
 }
 add_action( 'template_redirect', cb( 'redirect' ) );
 
-function log_error( \Throwable|\WP_Error|string $error ) {
-	if ( $error instanceof \Throwable ) {
-		error_log( $error->__toString() );
-	} else if ( $error instanceof \WP_Error ) {
-		foreach ( $error->get_error_codes() as $error_code ) {
-			$message = $error->get_error_message( $error_code );
-			error_log( "{$error_code}: {$message}" );
-		}
-	} else if ( is_string( $error ) ) {
-		error_log( $error );
+function filter_wc_title( string $title, int $post_id ): string {
+	if ( get_post_type( $post_id ) === 'product' ) {
+		return ltrim( str_replace( 'Transgression:', '', $title ) );
 	}
+	return $title;
 }
 
+function render_wc_clear_cart() {
+	printf(
+		'<p><a href="%s" class="clear-cart">Clear Cart</a></p>',
+		esc_url( add_query_arg( 'empty_cart', 'yes' ) )
+	);
+}
+
+function add_wc_title_prefix( WC_Product $product ): bool {
+	return strpos( $product->get_name(), 'Transgression:' ) === 0;
+}
