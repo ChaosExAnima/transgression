2d54a71e22937e44cfcf114f19dd1187cb976783
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2023-03-30 23:55:35 -0400

    Make logging modular

--- plugin/class-logger.php
+++ plugin/class-logger.php
@@ -2,13 +2,32 @@
 
 namespace Transgression;
 
+enum LoggerLevels: string {
+	case INFO = 'Info';
+	case WARNING = 'Warning';
+	case ERROR = 'Error';
+}
+
 class Logger {
+	public const ACTION_NAME = 'transgression_log';
+
+	public function __construct() {
+		add_action( self::ACTION_NAME, [ $this, 'php_log' ], 10, 2 );
+	}
+
 	public function log( mixed $message ) {
-		error_log( $this->to_string( $message ) );
+		do_action( self::ACTION_NAME, $this->to_string( $message ), LoggerLevels::INFO, $message );
 	}
 
 	public function error( mixed $error ) {
-		error_log( 'Error: ' . $this->to_string( $error ) );
+		do_action( self::ACTION_NAME, $this->to_string( $error ), LoggerLevels::ERROR, $error );
+	}
+
+	public function php_log( string $message, LoggerLevels $severity ) {
+		if ( $severity !== LoggerLevels::INFO ) {
+			$message = "{$severity->value}: {$message}";
+		}
+		error_log( $message );
 	}
 
 	protected function to_string( mixed $message ): string {
