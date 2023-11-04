e9f88bc7e55f5c9988d8907234352a10f948f1e8
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2023-11-04 16:54:12 -0400

    remove validation preventing editing old events

--- plugin/views/meta-event-time.php
+++ plugin/views/meta-event-time.php
@@ -24,7 +24,6 @@ wp_nonce_field( "event-time-{$post_id}", '_trans_event_nonce' );
 		name="start_time"
 		id="trans_start_time"
 		value="<?php echo esc_attr( substr( $start_time, 0, -6 ) ); ?>"
-		min="<?php echo esc_attr( wp_date( 'Y-m-d\TH:00' ) ); ?>"
 		step="1800"
 	/>
 </p>
@@ -36,28 +35,10 @@ wp_nonce_field( "event-time-{$post_id}", '_trans_event_nonce' );
 		name="end_time"
 		id="trans_end_time"
 		value="<?php echo esc_attr( substr( $end_time, 0, -6 ) ); ?>"
-		min="<?php echo esc_attr( wp_date( 'Y-m-d\TH:00' ) ); ?>"
 		step="1800"
 	/>
 </p>
 <p class="description">All times in <?php echo esc_html( wp_timezone_string() ); ?></p>
-<script type="module">
-	/** @type HTMLInputElement */
-	const startEle = document.getElementById('trans_start_time');
-	const endEle = document.getElementById('trans_end_time');
-	document.addEventListener('load', update, {once: true});
-	startEle.addEventListener('change', update);
-	endEle.addEventListener('change', update);
-	function update() {
-		if (startEle.value) {
-			endEle.setAttribute('min', startEle.value);
-		}
-		if (endEle.value) {
-			startEle.setAttribute('max', endEle.value);
-		}
-
-	}
-</script>
 <style>
 	.trans_event_time:invalid {
 		color: #b32d2e;
