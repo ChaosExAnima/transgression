b7c4fd294eb8e05ed2e1c17cf3f1c5a28dbad5ee
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-02-02 23:34:48 -0500

    Refactor ForbiddenTickets class and add new features

--- plugin/modules/class-forbidden-tickets.php
+++ plugin/modules/class-forbidden-tickets.php
@@ -1,4 +1,5 @@
 <?php declare( strict_types=1 );
+// @phpcs:disable WordPress.Security.NonceVerification
 
 namespace Transgression\Modules;
 
@@ -11,18 +12,41 @@ use const Transgression\PLUGIN_SLUG;
 
 class ForbiddenTickets extends Module {
 	public const USER_CODE_KEY = PLUGIN_SLUG . '_code';
+	public const OPTION_UNUSED_CODES = PLUGIN_SLUG . '_unused_codes';
 	public const CACHE_ALL_KEY = 'all_codes';
 
+	public const UNUSED_COUNT = 100;
+
+	protected Page_Options $admin;
+
 	public function __construct() {
-		$admin = new Page_Options( 'forbidden-tickets', 'Forbidden Tickets', 'Forbidden Tickets', [], 'manage_options' );
-		$admin->add_render_callback( [ $this, 'render' ], 1 );
+		$admin = new Page_Options( 'forbidden_tickets', 'Forbidden Tickets', 'Forbidden Tickets', [], 'manage_options' );
+		$this->admin = $admin;
+		$admin->add_action( 'generate_codes', [ $this, 'action_generate_codes' ] );
 		$admin->as_page( 'ticket.svg', 56 );
 		$admin->add_style( 'forbidden-tickets' );
 
-		( new Option( 'landing_url', 'Landing URL' ) )
-			->describe( 'The Forbidden Tickets landing page' )
-			->of_type( 'url' )
-			->on_page( $admin );
+		$admin->add_settings(
+			( new Option( 'copy_codes', 'Event codes' ) )
+				->of_type( 'none' )
+				->render_after( [ $this, 'render_copy' ] ),
+			( new Option( 'remaining_codes', 'Remaining codes' ) )
+				->of_type( 'none' )
+				->render_after( [ $this, 'render_remaining' ] ),
+			( new Option( 'producer_slug', 'Landing URL' ) )
+				->describe( 'The Forbidden Tickets producer slug' )
+				->on_page( $admin ),
+		);
+
+		$admin->register_message(
+			'codes_generated',
+			sprintf(
+				'Generated another %d codes. Copy and paste in <a href="%s">an event</a>.',
+				absint( $_GET['count'] ?? '' ),
+				$this->event_url( '/panel/pages/events+%s?tab=events' )
+			),
+			'success'
+		);
 	}
 
 	/**
@@ -31,26 +55,82 @@ class ForbiddenTickets extends Module {
 	 * @return void
 	 */
 	public function render(): void {
-		$codes = $this->get_all_codes();
-		load_view( 'forbidden-tickets/admin', compact( 'codes' ) );
+		load_view( 'forbidden-tickets/admin', [
+			'codes' => $this->all_codes(),
+			'unused_count' => count( $this->unused_codes() ),
+		] );
 	}
 
 	/**
-	 * Generate a code
+	 * Render the remaining codes
 	 *
-	 * @param bool $check_unique Check if the code is unique
-	 * @return string
+	 * @return void
 	 */
-	public function generate_code( bool $check_unique = true ): string {
-		$code = strtolower( wp_generate_password( 6, false ) );
-		if ( ! $check_unique ) {
-			return $code;
+	public function render_remaining() {
+		$unused_count = count( $this->unused_codes() );
+		$disabled = $unused_count === self::UNUSED_COUNT;
+		printf(
+			'<div class="flex-row"><span>%d</span>',
+			absint( $unused_count )
+		);
+		if ( ! $disabled || true ) {
+			$regen_url = $this->admin->get_url( [
+				'generate_codes' => '1',
+				'_wpnonce' => wp_create_nonce( 'regenerate_codes' ),
+			] );
+			printf(
+				'<a class="button button-secondary" href="%s">Regenerate</a>',
+				esc_url( $regen_url ),
+			);
+		} else {
+			echo '<a class="button button-disabled">Regenerate</p>';
 		}
-		$codes = $this->get_all_codes();
-		if ( in_array( $code, $codes, true ) ) {
-			return $this->generate_code();
+		echo '</div>';
+	}
+
+	public function render_copy() {
+		$all_codes = $this->all_codes();
+		printf(
+			'<div class="flex-row">
+				<code id="codes">%s<span class="dashicons dashicons-editor-paste-text"></span></code>
+				<p id="result" class="hidden copy-success">Successfully copied codes!</p>
+			</div>',
+			esc_textarea( implode( ',', $all_codes ) )
+		);
+	}
+
+	/**
+	 * Handle the generate codes button click
+	 *
+	 * @return void
+	 */
+	public function action_generate_codes() {
+		$generated = $this->generate_codes();
+		$this->admin->redirect_message( 'codes_generated', [ 'count' => $generated ] );
+	}
+
+	/**
+	 * Generate codes
+	 *
+	 * @return int
+	 */
+	protected function generate_codes(): int {
+		$unused_codes = $this->unused_codes();
+		$needed = self::UNUSED_COUNT - count( $unused_codes );
+		if ( $needed === 0 ) {
+			return 0;
 		}
-		return $code;
+		$all_codes = $this->all_codes();
+		$count = $needed;
+		while ( $needed > 0 ) {
+			$code = strtolower( wp_generate_password( 6, false ) );
+			if ( ! in_array( $code, $all_codes, true ) ) {
+				$unused_codes[] = $code;
+				--$needed;
+			}
+		}
+		update_option( self::OPTION_UNUSED_CODES, $unused_codes );
+		return $count;
 	}
 
 	/**
@@ -66,18 +146,33 @@ class ForbiddenTickets extends Module {
 			return $meta_value;
 		}
 
-		$code = $this->generate_code();
+		$codes = $this->unused_codes();
+		if ( count( $codes ) === 0 ) {
+			$this->generate_codes();
+			$codes = $this->unused_codes();
+		}
+		$code = array_shift( $codes );
 		update_user_meta( $user_id, self::USER_CODE_KEY, $code );
+		update_option( self::OPTION_UNUSED_CODES, $codes );
 		wp_cache_delete( self::CACHE_ALL_KEY, PLUGIN_SLUG );
 		return $code;
 	}
 
 	/**
-	 * Get all codes.
+	 * Get unused codes.
 	 *
 	 * @return array
 	 */
-	public function get_all_codes(): array {
+	public function unused_codes(): array {
+		return get_option( self::OPTION_UNUSED_CODES, [] );
+	}
+
+	/**
+	 * Get used codes.
+	 *
+	 * @return array
+	 */
+	public function used_codes(): array {
 		$cached = wp_cache_get( self::CACHE_ALL_KEY, PLUGIN_SLUG );
 		if ( $cached ) {
 			return $cached;
@@ -86,5 +181,23 @@ class ForbiddenTickets extends Module {
 		$codes = $wpdb->get_col( $wpdb->prepare( "SELECT meta_value FROM $wpdb->usermeta WHERE meta_key = %s", self::USER_CODE_KEY ) );
 		wp_cache_set( self::CACHE_ALL_KEY, $codes, PLUGIN_SLUG );
 		return $codes;
+	}
+
+	/**
+	 * Get all codes.
+	 *
+	 * @return array
+	 */
+	public function all_codes(): array {
+		$unused = $this->unused_codes();
+		$used = $this->used_codes();
+		return array_merge( $unused, $used );
+	}
+
+	protected function event_url( string $path ): string {
+		return sprintf(
+			"https://forbiddentickets.com{$path}",
+			$this->admin->get_setting( 'producer_slug' )->get()
+		);
 	}
 }
