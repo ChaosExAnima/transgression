c22d99cab304b3fc305f388526e5384af767b60d
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-03-07 22:51:13 -0500

    Add email sending functionality using ElasticEmail API

--- plugin/modules/emails/class-elasticemail.php
+++ plugin/modules/emails/class-elasticemail.php
@@ -5,6 +5,7 @@ namespace Transgression\Modules\Email;
 use Transgression\Admin\Option;
 use Transgression\Admin\Option_Select;
 use Transgression\Logger;
+use Transgression\Person;
 
 use function Transgression\prefix;
 
@@ -15,7 +16,50 @@ class ElasticEmail extends Email {
 	 * @inheritDoc
 	 */
 	protected function attempt_send(): bool {
-		return false;
+		if ( ! $this->template ) {
+			throw new \Error( 'No template set' );
+		}
+		$body = [
+			'Recipients' => [
+				'To' => [ $this->email ],
+			],
+			'Content' => [
+				'From' => sprintf(
+					'%s <%s>',
+					get_option( 'blogname' ),
+					get_option( 'admin_email' )
+				),
+				'Subject' => $this->subject,
+				'Merge' => [],
+				'TemplateName' => $this->template,
+				'Utm' => [
+					'Source' => sanitize_title( $this->template ),
+				],
+			],
+			'Options' => [
+				'Channel' => 'WordPress',
+			],
+		];
+		if ( $this->user ) {
+			$person = new Person( $this->user );
+			$body['Content']['Merge'] = [
+				'firstname' => $person->name(),
+				'ticket_code' => $person->code(),
+			];
+		}
+		$response = wp_remote_post( self::API_URL . '/emails/transactional', [
+			'body' => wp_json_encode( $body ),
+			'headers' => [
+				'Content-Type' => 'application/json',
+				'X-ElasticEmail-ApiKey' => $this->emailer->admin->value( 'elastic_email_key' ),
+			],
+		] );
+		if ( wp_remote_retrieve_response_code( $response ) !== 200 ) {
+			$body = wp_remote_retrieve_body( $response );
+			Logger::error( "Invalid response from EE API: {$body}" );
+			return false;
+		}
+		return true;
 	}
 
 	/**
@@ -98,9 +142,9 @@ class ElasticEmail extends Email {
 			return [];
 		}
 
-		$template_names = [ null ];
+		$template_names = [ '' => __( 'None', 'transgression' ) ];
 		foreach ( $json as $template ) {
-			$template_names[] = $template['Name'];
+			$template_names[ $template['Name'] ] = $template['Name'];
 		}
 		wp_cache_set( 'elastic_email_templates', $template_names, prefix( 'elastic_email' ), DAY_IN_SECONDS );
 		return $template_names;
